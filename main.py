1c066faf1e59a15b9bc68ab887e6c9503c6d139d
Allen Mao <excursion.allen@gmail.com>
2017-12-11 23:22:34 -0800

    232211DEC2017

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 # main.py
 import sys
-from conllu.parser import parse_tree
+from conllu.parser import parse
 if (len(sys.argv) != 2):
     raise Exception('Invalid Input')
-data = parse_tree(open(sys.argv[1], 'r').read())
+data = parse(open(sys.argv[1], 'r').read())
 print(data)
+
+head_to_tail = {}
+for word in data[0]:
+    if (word['head'] in head_to_tail):
+        head_to_tail[word['head']].append(word['id'])
+    else:
+        head_to_tail[word['head']] = [word['id']]
+#    head_to_tail[word['head']] = head_to_tail[word['head']].append(word['id'])
+    #print(head)
+print(head_to_tail)
+
